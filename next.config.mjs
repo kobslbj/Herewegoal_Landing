2bff46271a9c681a1db96de2754809c57997e766
JustinLi <kobs666666@gmail.com>
2025-05-05 19:39:47 +0800

    feat: Integrate MDX support for markdown files and update page extensions in Next.js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
+import createMDX from '@next/mdx';
+import remarkGfm from 'remark-gfm';
+
+const withMDX = createMDX({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [remarkGfm],
+  },
+});
+
 const nextConfig = {
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
 };
 
-export default nextConfig;
+export default withMDX(nextConfig);
